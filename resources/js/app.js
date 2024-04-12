e321f1df8bb7da6830b7557b15b98256b6bab9b9
Miguel Gaspar <miguelgaspar2010@gmail.com>
2024-04-12 13:45:59 +0100

    Add example-component.vue and update app.js and vite.config.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from "vue";
+import ExampleComponent from "./Components/example-component.vue";
+
+const app = createApp({});
+
+
+
+app.component("example-component", ExampleComponent);
+
+app.mount("#app");
